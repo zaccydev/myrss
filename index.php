822bc953bbfdd663685f1a2fe6456fb2d5832ec1
zaccydev <pbosquet@free.fr>
2022-11-08 08:48:03 +0100

    nd

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
-  error_reporting(-1);
-  ini_set("display_errors", 1);
+error_reporting(-1);
+ini_set("display_errors", 1);
 
-  $flux_rss = "./rss.xml"; 
+$flux_rss = "./rss.xml"; 
 //"https://www.courrierinternational.com/feed/all/rss.xml";
 
-  $xml = simplexml_load_file($flux_rss);
-  //print_r ($xml);
-  
+$xml = simplexml_load_file($flux_rss);
 
-  print $xml['rss']['channel']['title'];
+
+print $xml->channel->title;
+echo "<br/>";
+print $xml->channel->pudDate;
+echo "<br/>";
+foreach ($xml->channel->item as $elt => $art) {
+    $ns = $art->getNamespaces(true);
+    $childM = $art->children($ns['media']);
+    print $art->title;
+    echo "<br/>";
+    print "<a href=\"$art->link\">lire</a>";
+    echo "<br/>";
+    print $art->description;
+    echo "<br/>";
+    //var_dump($childM);
+    
+    echo "<br/>";
+}
